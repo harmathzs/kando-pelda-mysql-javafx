b93d3fef6708750c4bd6734da7fbd9ab2614c37a
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 13:19:45 +0200

    Add Owner class

---
+++ src/main/java/org/example/kandopeldamysqljavafx/Owner.java
@@ -0,0 +1,28 @@
+package org.example.kandopeldamysqljavafx;
+
+public class Owner {
+    private Integer id;
+    private String name;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+
+    public Owner(Integer id, String name) {
+        setId(id);
+        setName(name);
+    }
+}
+
