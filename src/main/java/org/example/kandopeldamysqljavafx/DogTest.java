8d8de7020e395f61691f1e149518d1446b04a5df
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 13:30:57 +0200

    Add DogTest

---
+++ src/main/java/org/example/kandopeldamysqljavafx/DogTest.java
@@ -0,0 +1,24 @@
+package org.example.kandopeldamysqljavafx;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DogTest {
+    @Test
+    public void testDog() {
+        Dog fido = new Dog(
+                1, "Fido",
+                3, true,
+                null
+        );
+
+        //Assert.assertEquals(1, fido.getId());
+        int fidoId = fido.getId();
+        Assert.assertEquals(1, fidoId);
+
+        Assert.assertEquals("Fido", fido.getName());
+        Assert.assertEquals(3, fido.getAge(), 0.2);
+        Assert.assertTrue(fido.isMale());
+        Assert.assertNull(fido.getOwner());
+    }
+}
