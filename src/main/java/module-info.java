4ea887ef5ff620540ef27db207e5b9fb3c342ca4
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 13:28:33 +0200

    Implement OwnerTest

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.kandopeldamysqljavafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires junit;
 
 
     opens org.example.kandopeldamysqljavafx to javafx.fxml;
