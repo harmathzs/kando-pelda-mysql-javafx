121f697756c593272110bfedc93d07b1df8d5239
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 13:14:45 +0200

    Rename hello to Dogs and Owners

--- src/main/java/org/example/kandopeldamysqljavafx/DogsOwnersApplication.java
+++ src/main/java/org/example/kandopeldamysqljavafx/DogsOwnersApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class DogsOwnersApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DogsOwnersApplication.class.getResource("dogsowners-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Dogs and Owners!");
         stage.setScene(scene);
         stage.show();
     }
