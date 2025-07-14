088d0148b10a548eb9fbedbac7b3fe23985d0827
Zsolt Harmath <harmathzs@gmail.com>
2025-07-14 13:23:18 +0200

    Add MysqlService class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.kandopeldamysqljavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.kandopeldamysqljavafx to javafx.fxml;
