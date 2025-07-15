bf3980cd76a7dce67501cdb3862c4b0b4e57858d
Zsolt Harmath <harmathzs@gmail.com>
2025-07-15 13:08:19 +0200

    Name components

--- src/main/java/org/example/kandopeldamysqljavafx/DogsOwnersController.java
+++ src/main/java/org/example/kandopeldamysqljavafx/DogsOwnersController.java
@@ -1,14 +1,52 @@
 package org.example.kandopeldamysqljavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class DogsOwnersController {
+    @FXML public TableView dogsTableView;
+    @FXML public TableView ownersTableView;
+    @FXML public Spinner dogIdSpinner;
+    @FXML public TextField dogNameTextField;
+    @FXML public Spinner dogAgeSpinner;
+    @FXML public ChoiceBox dogMaleChoiceBox;
+    @FXML public Spinner dogOwneridSpinner;
+    @FXML public Spinner ownerIdSpinner;
+    @FXML public TextField ownerNameTextField;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onButton1Click() {
+        //
+    }
+    @FXML
+    protected void onButton2Click() {
+        //
+    }
+    @FXML
+    protected void onButton3Click() {
+        //
+    }
+    @FXML
+    protected void onButton4Click() {
+        //
+    }
+    @FXML
+    protected void onButton5Click() {
+        //
+    }
+    @FXML
+    protected void onButton6Click() {
+        //
+    }
+    @FXML
+    protected void onButton7Click() {
+        //
     }
 }
